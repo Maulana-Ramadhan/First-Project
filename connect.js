456bacd6b1236a26f51c85f5d9ed3791f37050c4
Maulana-Ramadhan <maulanaramadhan3010@gmail.com>
2024-09-05 12:24:28 +0700

    update connect.js

--- connect.js
+++ connect.js
@@ -1,7 +1,7 @@
 console.log("walaw");
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-analytics.js";
-import { getDatabase, ref, set, get} from "https://www.gstatic.com/firebasejs/10.13.1/firebase-database.js";
+import { getDatabase, ref, set, get, onValue} from "https://www.gstatic.com/firebasejs/10.13.1/firebase-database.js";
 import { getAuth, signInAnonymously, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
 const firebaseConfig = {
     apiKey: "AIzaSyAY2bOvEZnMlNOSuJAHjHXA38LXyca1qG0",
@@ -18,10 +18,10 @@ const analytics = getAnalytics(app);
 const database = getDatabase(app);
 const auth = getAuth(app);
 
+  const dataRef = ref(database, 'data/' + auth.currentUser.uid);
 signInAnonymously(auth)
 .then(pass => {
   console.log(pass);
-  const dataRef = ref(database, 'data/' + user.uid);
   function writeUserData(userId, name, email, imageUrl) {
     set(ref, {
       username: name,
