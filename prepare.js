b2dd9688d5d8e98405e0c7b07e9347b537ed7340
Maulana-Ramadhan <maulanaramadhan3010@gmail.com>
2024-09-06 13:09:43 +0700

    update prepare.js

--- prepare.js
+++ prepare.js
@@ -1,21 +1,10 @@
 const playersName = {};
 
 fbg.onAuthStateChanged(fbg.auth, check => {
-  if (check) {
-    console.log("user");
-    fbg.onValue(fbg.ref(fbg.database, `data/users/${check.user.uid}/name`), (sp) => {
-      playersName[check.user.uid] = sp.val();
-      console.log("value");
-    });
-    console.log("user");
-  } else {
-  }
-});
-fbg.onAuthStateChanged(fbg.auth, check => {
   console.log(check);
   if (check) {
     fbg.onValue(fbg.ref(fbg.database, `data/users/${check.uid}/name`), sp => {
-      playersName[check.user.id] = sp.val();
+      playersName[check.uid] = sp.val();
       console.log("value");
     });
   }
