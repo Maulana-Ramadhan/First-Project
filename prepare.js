279762398dc6cd33e5f8fef5da6be972f6d9b320
Maulana-Ramadhan <maulanaramadhan3010@gmail.com>
2024-09-06 11:20:57 +0700

    update prepare.js

--- prepare.js
+++
@@ -1,6 +0,0 @@
-function move(pl,pos) {
-  ele.pl.style.transform = `translate(${pos[0]},${pos[1]})`;
-}
-const elementPlayer = localStorage.get("elementPlayer") || {};
-if (elementPlayer?.uid) prompt("masukkan namamu: ")
-document.body.appendChild();
