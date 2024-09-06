68d19414288054ec35b3476e3461a678ec23735f
Maulana-Ramadhan <maulanaramadhan3010@gmail.com>
2024-09-06 11:39:29 +0700

    update prepare.js

--- prepare.js
+++ prepare.js
@@ -1,5 +1,6 @@
 
-
+console.log(fbg);
+console.log(window.fbg);
 fbg.onAuthStateChanged(auth, user => {
   if (user) {
     onValue(ref(database, `data/users/${pass.user.uid}/name`), (sp) => playersName[pass.user.uid] = sp.val());
