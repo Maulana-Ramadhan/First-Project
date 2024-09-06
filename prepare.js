b868e5d6e0a42eb951584815ae3ce832305e9aaf
Maulana-Ramadhan <maulanaramadhan3010@gmail.com>
2024-09-06 08:45:09 +0700

    Create prepare.js

---
+++ prepare.js
@@ -0,0 +1,6 @@
+function move(pl,pos) {
+  ele.pl.style.transform = `translate(${pos[0]},${pos[1]})`;
+}
+const elementPlayer = localStorage.get("elementPlayer") || {};
+if (elementPlayer?.uid) prompt("masukkan namamu: ")
+document.body.appendChild();
