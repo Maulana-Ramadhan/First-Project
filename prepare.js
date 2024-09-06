1820149f6c88799acf440a954fad3477e1d3172d
Maulana-Ramadhan <maulanaramadhan3010@gmail.com>
2024-09-06 22:34:54 +0700

    update prepare.js

--- prepare.js
+++ prepare.js
@@ -1,7 +1,7 @@
 const playersName = {};
 let count = 0;
 fbg.onAuthStateChanged(fbg.auth, check => {
-  console.log(count++);
+  console.log(check);
   if (check) {
     fbg.onValue(fbg.ref(fbg.database, `data/users/${check.uid}/name`), sp => {
       playersName[check.uid] = sp.val();
