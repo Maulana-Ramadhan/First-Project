d7bcc4c54372388a3dffa066510dc0f455fceac5
Maulana-Ramadhan <maulanaramadhan3010@gmail.com>
2024-09-06 11:22:59 +0700

    update connect.js

--- connect.js
+++ connect.js
@@ -13,10 +13,6 @@
 //     appId: "1:330858606744:web:d36923fdbb80e5932020ed",
 //     measurementId: "G-CSFLLP262Z"
 // };
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-// const database = getDatabase(app);
-// const auth = getAuth(app);
 
 // signInAnonymously(auth)
 // .then(pass => {
@@ -65,9 +61,15 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/fireba
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-analytics.js";
 import { getDatabase, ref, set, get, onValue, child, push, update } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-database.js";
 import { getAuth, signInAnonymously, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const database = getDatabase(app);
+const auth = getAuth(app);
 window.fbg = {
   initializeApp,
   getAnalytics,
   getDatabase, ref, set, get, onValue, child, push, update,
   getAuth, signInAnonymously, onAuthStateChanged,
+  
+  app, analytics, database, auth,
 };
