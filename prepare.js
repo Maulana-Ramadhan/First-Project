527705d894c83023137a192d136c69e881007a9a
Maulana-Ramadhan <maulanaramadhan3010@gmail.com>
2024-09-06 22:31:48 +0700

    update prepare.js

--- prepare.js
+++ prepare.js
@@ -2,13 +2,11 @@ const playersName = {};
 let count = 0;
 fbg.onAuthStateChanged(fbg.auth, check => {
   console.log(count++);
-  const userRef = fbg.ref(fbg.database, `data/users/${check.uid}/name`);
   if (check) {
-    fbg.onValue(userRef, sp => {
+    fbg.onValue(fbg.ref(fbg.database, `data/users/${check.uid}/name`), sp => {
       playersName[check.uid] = sp.val();
       playerListMenu.innerHTML += `<div>${playersName[check.uid]}</div><br/>`;
     });
-    userRef.onDisconnect().remove();
   }
 });
 fbg.signInAnonymously(fbg.auth).then(pass => {
