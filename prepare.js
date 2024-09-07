1e2a5fa43ca064c025e16d310ea4f1f3e1ec6b78
Maulana-Ramadhan <maulanaramadhan3010@gmail.com>
2024-09-07 21:23:34 +0700

    update prepare.js

--- prepare.js
+++ prepare.js
@@ -1,6 +1,10 @@
-const rName = ["red","green","blue","ivory","pink","olive","black","coral","fuchsia","teal","saddlebrown","tan"];
+const me = function() {
+  const rName = ["red","green","blue","ivory","pink","olive","black","coral","fuchsia","teal","saddlebrown","tan"];
+  return rName[Math.floor(Math.random()*12)];
+}();
+
 
 fbg.signInAnonymously(fbg.auth).catch(error => console.error(error.message));
 fbg.onAuthStateChanged(fbg.auth, user => {
-  fbg.set(fbg.ref(fbg.database, 'data/users/' + user.uid), [0,0,rName[Math.floor(Math.random()*12)]]);
+  fbg.set(fbg.ref(fbg.database, 'data/users/' + user.uid), [0,0,]);
 });
