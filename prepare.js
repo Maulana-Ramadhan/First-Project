7e8a93bd8612afab7085f40b2608862b9788b23f
Maulana-Ramadhan <maulanaramadhan3010@gmail.com>
2024-09-07 21:26:24 +0700

    update prepare.js

--- prepare.js
+++ prepare.js
@@ -6,3 +6,6 @@ fbg.signInAnonymously(fbg.auth).catch(error => console.error(error.message));
 fbg.onAuthStateChanged(fbg.auth, user => {
   fbg.set(fbg.ref(fbg.database, 'data/users/' + user.uid), [0,0,myColor]);
 });
+fbg.onValue(fbg.ref(database, 'data/thatIn'), (sp) => {
+  
+});
