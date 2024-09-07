7468b932e978498c616560688cd13c819b6e9b26
Maulana-Ramadhan <maulanaramadhan3010@gmail.com>
2024-09-07 22:21:07 +0700

    update prepare.js

--- prepare.js
+++ prepare.js
@@ -12,16 +12,22 @@ fbg.onValue(fbg.ref(fbg.database, 'data/thatIn'), (sp) => {
     el.classList.add("players");
     el.style.backgroundColor = sp.child(thid).val();
     document.body.appendChild(el);
+    window.waka = sp;
     fbg.onValue(fbg.ref(fbg.database, 'data/users/' + thid + '/pos'), (spm) => {
       el.style.transform = `translate(${spm[0]}px,${spm[1]}px)`;
     });
   }
 });
 fbg.onAuthStateChanged(fbg.auth, (user) => {
-  me = user.uid;
+  let me = user.uid;
   if (user) {
     fbg.set(fbg.ref(fbg.database, 'data/users/' + user.uid), [0,0]);
     fbg.set(fbg.ref(fbg.database, 'data/thatIn/' + user.uid), myColor);
+    const el = document.createElement("div");
+    el.id = thid;
+    el.classList.add("players");
+    el.style.backgroundColor = sp.child(thid).val();
+    document.body.appendChild(el);
   } else {
     fbg.set(fbg.ref(fbg.database, 'data/thatIn/' + user.uid), false);
   }
