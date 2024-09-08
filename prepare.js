9452dadbae83abb4e0825bb20ac7a82f6986f47d
Maulana-Ramadhan <maulanaramadhan3010@gmail.com>
2024-09-08 23:48:41 +0700

    this

--- prepare.js
+++ prepare.js
@@ -1,5 +1,8 @@
 const muid = [];
-const real = [0,0];
+const real = {
+  position: [0,0],
+  direction: 0,
+};
 const elPlayers = {};
 fbg.signInAnonymously(fbg.auth).then( a => {
   muid.push(localStorage.getItem("myName") || prompt("isi namamu:"));
@@ -15,40 +18,54 @@ fbg.onAuthStateChanged(fbg.auth, (user) => {
     el.style.backgroundColor = rName[Math.floor(Math.random()*12)];
     document.body.appendChild(el);
     elPlayers[user.uid] = el;
-    fbg.set(fbg.ref(fbg.database, 'data/users/' + user.uid), [0,0]);
-    fbg.set(fbg.ref(fbg.database, 'data/thatIn/' + user.uid), myColor);
+    fbg.set(fbg.ref(fbg.database, 'data/users/' + user.uid), {
+      position: [0,0],
+      direction: 0,
+    });
+    fbg.set(fbg.ref(fbg.database, 'data/thatIn/' + user.uid), {
+      status: true,
+      name: muid[0],
+      uid: muid[1],
+      color: el.style.backgroundColor,
+    });
+    fbg.get(fbg.ref(fbg.database, 'data/thatIn/'), (sp) => {
+      for (const i in sp.val()) { if (i.status) {
+        const el = document.createElement("div");
+        el.id = i.uid;
+        el.classList.add("players");
+        el.style.backgroundColor = i.color;
+        document.body.appendChild(el);
+        fbg.onValue(fbg.ref(fbg.database, 'data/users/' + i.uid), (spm) => {
+          const me = spm.val();
+          el.style.transform = `translate(${me.position[0]}px,${me.position[1]}px) rotate(${me.direction}deg)`;
+        });
+      }}
+    })
+    fbg.onValue(fbg.ref(fbg.database, 'data/thatIn/'), (sp) => {
+      const thid = sp._node.children_.root_.key;
+      if (muid[1] != thid) {
+        const el = document.createElement("div");
+        el.id = thid;
+        el.classList.add("players");
+        el.style.backgroundColor = sp.child(thid).val();
+        document.body.appendChild(el);
+        fbg.onValue(fbg.ref(fbg.database, 'data/users/' + thid), (spm) => {
+          const me = spm.val();
+          el.style.transform = `translate(${me.position[0]}px,${me.position[1]}px) rotate(${me.direction}deg)`;
+        });
+      }
+    });
   } else {
-    fbg.set(fbg.ref(fbg.database, 'data/thatIn/' + user.uid), false);
+    fbg.set(fbg.ref(fbg.database, 'data/thatIn/' + user.uid + 'status'), false);
   }
 });
 analog.addEventListener("click", a => {
   switch (a.target.id) {
-    case 'atasAnalog': real[1] -= 50; break;
-    case 'kananAnalog': real[0] += 50; break;
-    case 'bawahAnalog': real[1] += 50; break;
-    case 'kiriAnalog': real[0] -= 50; break;
+    case 'atasAnalog': real.position[1] -= 50; real.direction = 0; break;
+    case 'kananAnalog': real.position[0] += 50; real.direction = 90; break;
+    case 'bawahAnalog': real.position[1] += 50; real.direction = 180; break;
+    case 'kiriAnalog': real.position[0] -= 50; real.direction = -90; break;
   }
   fbg.set(fbg.ref(fbg.database, 'data/users/' + muid[1]), real);
-  console.log(elPlayers[muid[1]]);
-  
-  elPlayers[muid[1]].style.transform = `translate(${real[0]}px,${real[1]}px)`;
-});
-(function() {
-  
-}());
-
-fbg.onValue(fbg.ref(fbg.database, 'data/thatIn'), (sp) => {
-  const thid = sp._node.children_.root_.key;
-  console.log(elPlayers);
-  if (muid[1] != thid) {
-    const el = document.createElement("div");
-    el.id = thid;
-    el.classList.add("players");
-    el.style.backgroundColor = sp.child(thid).val();
-    document.body.appendChild(el);
-    fbg.onValue(fbg.ref(fbg.database, 'data/users/' + thid), (spm) => {
-      console.log(el,spm);
-      el.style.transform = `translate(${spm.val()[0]}px,${spm.val()[1]}px)`;
-    });
-  }
+  elPlayers[muid[1]].style.transform = `translate(${real.position[0]}px,${real.position[1]}px) rotate(${real.direction}deg)`;
 });
