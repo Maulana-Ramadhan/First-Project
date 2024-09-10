6582322694f6ba6f0d3fba8e4ce0b4d859746c78
Maulana-Ramadhan <maulanaramadhan3010@gmail.com>
2024-09-10 20:57:22 +0700

    Update prepare.js

--- prepare.js
+++ prepare.js
@@ -76,7 +76,7 @@ fbg.onAuthStateChanged(fbg.auth, (user) => {
     });
     fbg.get(fbg.ref(fbg.database, 'data/thatIn')).then((sp) => {
       window.wkwk = sp;
-      sp.forEach( i => { i = i.val(); if (i.status && i.uid == muid[1]) {
+      sp.forEach( i => { i = i.val(); if (i.status && i.uid != muid[1]) {
         const el = document.createElement("div");
         el.id = i.uid;
         el.classList.add("players");
