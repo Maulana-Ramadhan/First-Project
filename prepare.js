9b71ced294169c55dc042f39c0f0f1c7efa221cc
Maulana-Ramadhan <maulanaramadhan3010@gmail.com>
2024-09-10 22:25:11 +0700

    s

--- prepare.js
+++ prepare.js
@@ -35,6 +35,8 @@ if(localStorage.getItem("myData")) {
   muid[0] = myData.name;
   muid[1] = myData.uid;
   muid[2] = myData.color;
+  containerFullScreen.value = muid[0];
+  PickColor.value = muid[2];
   const el = document.createElement("div");
   el.id = muid[0];
   el.classList.add("players");
