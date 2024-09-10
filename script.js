ba4330d4156db2a96fafe6837de2d9ae9004d1e5
Maulana-Ramadhan <maulanaramadhan3010@gmail.com>
2024-09-10 21:15:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -45,19 +45,20 @@ function main() {
   }
   function whoch() {
     switch(touched.key) {
-      case 'w': real.position[1] -= settings.size; real.direction = 0; break;
-      case 'd': real.position[0] += settings.size; real.direction = 90; break;
-      case 's': real.position[1] += settings.size; real.direction = 180; break;
-      case 'a': real.position[0] -= settings.size; real.direction = 270; break; 
+      case 'w': real.position[1] -= settings.size; real.direction = "front"; break;
+      case 'd': real.position[0] += settings.size; real.direction = "right"; break;
+      case 's': real.position[1] += settings.size; real.direction = "back"; break;
+      case 'a': real.position[0] -= settings.size; real.direction = "left"; break; 
     }
     fbg.set(fbg.ref(fbg.database, 'data/users/' + muid[1]), real);
     elPlayers[muid[1]].style.transform = `translate(${real.position[0]}px, ${real.position[1]}px)`;
     elPlayers[muid[1]].style.rotate = `${real.direction}deg`;
+    console.log("tes");
   }
-  document.addEventListener('keydown', (e) => { 
-    touched.key = e.key;
-    whoch();
+  document.addEventListener('keydown', (e) => { if (!touched.key) {
     elPlayers[muid[1]].addEventListener("transitionend", whoch);
+    whoch(); }
+    touched.key = e.key; 
   });
   document.addEventListener('keyup', (e) => {
     touched.key = undefined;
@@ -80,12 +81,12 @@ function main() {
   });
   document.addEventListener('keydown', (e) => { 
     switch (e.key) {
-      case 'u': intr1(); return;
-      case 'i': intr2(); return;
-      case 'o': intr3(); return;
-      case 'h': intr4(); return;
-      case 'j': intr5(); return;
-      case 'k': intr6(); return;
+      case 'u': intr1(e); return;
+      case 'i': intr2(e); return;
+      case 'o': intr3(e); return;
+      case 'h': intr4(e); return;
+      case 'j': intr5(e); return;
+      case 'k': intr6(e); return;
     }
   });
   bIntr1.addEventListener('touchdown', intr1);
