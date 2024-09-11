a68dc9761db344abcf994abeba2106374711124e
Maulana-Ramadhan <maulanaramadhan3010@gmail.com>
2024-09-11 12:09:52 +0700

    update prepare.js

--- prepare.js
+++ prepare.js
@@ -31,9 +31,9 @@ const settings = {
 };
 const elPlayers = {};
 function moveDirection(a,el) { el.style.borderRadius = "0"; switch (a) {
-    case 'front': el.style.borderTopLeftRadius = "100px"; el.style.borderTopRightRadius = "100px"; return;
+    case 'back': el.style.borderTopLeftRadius = "100px"; el.style.borderTopRightRadius = "100px"; return;
     case 'right': el.style.borderTopRightRadius = "100px"; el.style.borderBottomRightRadius = "100px"; return;
-    case 'back': el.style.borderBottomRightRadius = "100px"; el.style.borderBottomLeftRadius = "100px"; return;
+    case 'front': el.style.borderBottomRightRadius = "100px"; el.style.borderBottomLeftRadius = "100px"; return;
     case 'left': el.style.borderBottomLeftRadius = "100px"; el.style.borderTopLeftRadius = "100px"; return;
   }}
 if(localStorage.getItem("myData")) {
