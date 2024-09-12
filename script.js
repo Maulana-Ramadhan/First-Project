5b190681a6843b284260c4428941ab701e55031b
Maulana-Ramadhan <maulanaramadhan3010@gmail.com>
2024-09-12 13:31:38 +0700

    update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function main() {
   const varM = {
     mapL: [0,parseInt(MainGame.offsetWidth),parseInt(MainGame.offsetHeight),0],
+    mapOs: [0,0]
     x: 0,
     y: 0,
     ax: analog.getBoundingClientRect().left + (analog.getBoundingClientRect().width/2),
@@ -39,15 +40,15 @@ function main() {
     elPlayers[muid[1]].style.transform = `translate(${real.position[0]}px, ${real.position[1]}px)`;
     console.log(varM.x,varM.mapL[3],varM.mapL[2]);
     if (varM.x < varM.mapL[3]) {
-      MainGame.style.transform = `translateX(${varM.mapL[0]+=50}px)`;
+      MainGame.style.transform = `translateX(${varM.mapL[]+=50}px)`;
       console.log("<");
     }
     else if (varM.x > varM.mapL[1]) {
       MainGame.style.transform = `translateX(${varM.mapL[0]-=50}px)`;
       console.log(">");
     }
-    if (varM.y < varM.mapL[2]) MainGame.style.transform = `translateX(${varM.mapL[0]+=50}px)`;
-    else if (varM.y > varM.mapL[0]) MainGame.style.transform = `translateX(${varM.mapL[0]-=50}px)`;
+    if (varM.y < varM.mapL[2]) MainGame.style.transform = `translateY(${varM.mapL[0]+=50}px)`;
+    else if (varM.y > varM.mapL[0]) MainGame.style.transform = `translateY(${varM.mapL[0]-=50}px)`;
   }
   function which() {
     const c = (varM.x**2+varM.y**2)**(1/2);
