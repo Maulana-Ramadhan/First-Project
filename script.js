b29d6757659b1af692bab67e95e7c89fbe090551
Maulana-Ramadhan <maulanaramadhan3010@gmail.com>
2024-09-12 14:43:52 +0700

    update script.js

--- script.js
+++ script.js
@@ -43,8 +43,8 @@ function main() {
     fbg.set(fbg.ref(fbg.database, 'data/users/' + muid[1]), real); 
     moveDirection(real.direction,elPlayers[muid[1]]);
     elPlayers[muid[1]].style.transform = `translate(${real.position[0]}px, ${real.position[1]}px)`;
-    console.log("x",varM.x,varM.mapLi[3],varM.mapLi[1]);
-    console.log("y",varM.y,varM.mapLi[0],varM.mapLi[2]);
+    console.log("x",real.position[0],varM.mapLi[3],varM.mapLi[1]);
+    console.log("y",real.position[1],varM.mapLi[0],varM.mapLi[2]);
     if (real.position[0] < varM.mapLi[3]) {
       MainGame.style.transform = `translateX(${varM.mapLP(0,3,+1)}px)`;
       console.log("<");
