49e06f98c8646d5894763f748e8b7d98a36cdd5b
Maulana-Ramadhan <maulanaramadhan3010@gmail.com>
2024-09-17 11:09:40 +0700

    update mapCreate.js

--- mapCreate.js
+++ mapCreate.js
@@ -1,38 +1,35 @@
 const enCodetoObject = ["air","solid","door","chest"];
 const configuration = ["choosedPlace","allowRotate"];
-function conStructor(name,enCodetoObject,configuration) {
-  
-}
 const structur = {
-  basicSquare: {
-    enCodetoObject: [
-      [1, 1, 1, 1, 1],
-      [1, 0, 0, 0, 1],
-      [1, 0, 0, 0, 1],
-      [1, 0, 0, 0, 1],
-      [1, 1, 2, 1, 1],
-    ],
-    configuration: [
-      [1], 
-    ],
-  },
-  treasureRoom: {
-    enCodetoObject: [
-      [1, 1, 1, 1, 2, 1, 1, 1, 1],
-      [1, 0, 0, 0, 0, 0, 0, 0, 1],
-      [1, 0, 1, 1, 0, 1, 1, 0, 1],
-      [1, 0, 1, 0, 0, 0, 1, 0, 1],
-      [2, 0, 0, 0, 3, 0, 0, 0, 2],
-      [1, 0, 1, 0, 0, 0, 1, 0, 1],
-      [1, 0, 1, 1, 0, 1, 1, 0, 1],
-      [1, 0, 0, 0, 0, 0, 0, 0, 1],
-      [1, 1, 1, 1, 2, 1, 1, 1, 1],
-    ],
-    configuration: [
-      [0,[4,36,44,76],[4],[1]] //[configuration,[semua tiles yang diganti],[minimal yang harus diganti],[dengan tiles apa jika tidak diganti],[[tiles yang diacak],[bobot setiap tiles]]]
-      [1],
-    ],
-  },
+  // basicSquare: {
+  //   enCodetoObject: [
+  //     [1, 1, 1, 1, 1],
+  //     [1, 0, 0, 0, 1],
+  //     [1, 0, 0, 0, 1],
+  //     [1, 0, 0, 0, 1],
+  //     [1, 1, 2, 1, 1],
+  //   ],
+  //   configuration: [
+  //     [1], 
+  //   ],
+  // },
+  // treasureRoom: {
+  //   enCodetoObject: [
+  //     [1, 1, 1, 1, 2, 1, 1, 1, 1],
+  //     [1, 0, 0, 0, 0, 0, 0, 0, 1],
+  //     [1, 0, 1, 1, 0, 1, 1, 0, 1],
+  //     [1, 0, 1, 0, 0, 0, 1, 0, 1],
+  //     [2, 0, 0, 0, 3, 0, 0, 0, 2],
+  //     [1, 0, 1, 0, 0, 0, 1, 0, 1],
+  //     [1, 0, 1, 1, 0, 1, 1, 0, 1],
+  //     [1, 0, 0, 0, 0, 0, 0, 0, 1],
+  //     [1, 1, 1, 1, 2, 1, 1, 1, 1],
+  //   ],
+  //   configuration: [
+  //     [0,[4,36,44,76],[4],[1]] //[configuration,[semua tiles yang diganti],[minimal yang harus diganti],[dengan tiles apa jika tidak diganti],[[tiles yang diacak],[bobot setiap tiles]]]
+  //     [1],
+  //   ],
+  // },
   prison: {
     enCodetoObject: [
       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -59,6 +56,7 @@ const structur = {
     rotate90(){return this.enCodetoObject.toReversed().map((k,i)=>k.map((l,j)=>this.enCodetoObject[j][i]))},
     rotate180(){return this.enCodetoObject.toReversed().map((k,i)=>k.toReversed().map((l,j)=>this.enCodetoObject[i][j]))},
     rotate270(){return this.enCodetoObject.map((k,i)=>k.toReversed().map((l,j)=>this.enCodetoObject[j][i]))},
+    
   },
   conStructor(name,enCodetoObject,configuration) {
     this[name] = {
@@ -68,6 +66,41 @@ const structur = {
     Object.assign(this[name], this.metStructor);
   }
 };
+structur.conStructor("basicSquare",[
+  [1, 1, 1, 1, 1],
+  [1, 0, 0, 0, 1],
+  [1, 0, 0, 0, 1],
+  [1, 0, 0, 0, 1],
+  [1, 1, 2, 1, 1],],
+  [[1],]
+);
+structur.conStructor("treasureRoom",[
+  [1, 1, 1, 1, 2, 1, 1, 1, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 1, 1, 0, 1, 1, 0, 1],
+  [1, 0, 1, 0, 0, 0, 1, 0, 1],
+  [2, 0, 0, 0, 3, 0, 0, 0, 2],
+  [1, 0, 1, 0, 0, 0, 1, 0, 1],
+  [1, 0, 1, 1, 0, 1, 1, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 1, 1, 1, 2, 1, 1, 1, 1],],
+  [[0,[4,36,44,76],[4],[1]] //[configuration,[semua tiles yang diganti],[minimal yang harus diganti],[dengan tiles apa jika tidak diganti],[[tiles yang diacak],[bobot setiap tiles]]]
+  [1],],
+);
+structur.conStructor("prison",[
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1],
+  [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1],
+  [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1],
+  [1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1],
+  [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+  [1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1, 1, 2, 1, 1],
+  [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1],
+  [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1],
+  [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1],
+  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],],
+  [[1]]
+);
 const specialArrayMap = (a,x,y) => structur[a][y][x];
 function createMAp() {
   
