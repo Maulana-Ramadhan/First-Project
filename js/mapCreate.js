46f9eaea67c031fdcf4c8fd73f7ab12b3d33e898
Maulana-Ramadhan <maulanaramadhan3010@gmail.com>
2024-10-11 10:20:00 +0700

    update js/mapCreate.js

--- js/mapCreate.js
+++ js/mapCreate.js
@@ -21,12 +21,13 @@ const structure = {
     this[name] = {
       enCodetoObject: proto[0],
       configuration: proto[1],
+      limit: proto[2],
       wall: {
-        front: proto[2]||undefined,
-        right: proto[3]||undefined,
-        back: proto[4]||undefined,
-        left: proto[5]||undefined,
-      }
+        front: proto[3]||undefined,
+        right: proto[4]||undefined,
+        back: proto[5]||undefined,
+        left: proto[6]||undefined,
+      },
     };
     Object.assign(this[name], this.metStructor);
   }
