f93ac69ae324e197e1880c64090c82c52da96185
Maulana-Ramadhan <maulanaramadhan3010@gmail.com>
2024-10-11 08:38:25 +0700

    update js/mapCreate.js

--- js/mapCreate.js
+++ js/mapCreate.js
@@ -122,7 +122,7 @@ structure.conStructorB(("treasureRoom"),[[
   [[0,[[4,0],[0,5],[8,5]],[50,50,50],[2]], //[configuration,[semua tiles yang diganti],[minimal, maksimal | yang harus diganti],[dengan tiles apa jika tidak diganti],[[tiles yang diacak],[bobot setiap tiles]]]
   [1,[true,true,true,true]],],
 ]);
-structure.conStructor(("prison"),[[
+structure.conStructorB(("prison"),[[
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
   [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1],
   [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1],
