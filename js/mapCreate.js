5e35086341212f84a92f30bff37d9c73e39cfd4a
Maulana-Ramadhan <maulanaramadhan3010@gmail.com>
2024-10-12 21:38:26 +0700

    update js/mapCreate.js

--- js/mapCreate.js
+++ js/mapCreate.js
@@ -30,41 +30,10 @@ NFSTR.prototype.conStructorR = function(name,proto) {
         back: proto[5]||undefined,
         left: proto[6]||undefined,
       },
+      walls: proto[7] || undefined,
     };
     Object.assign(this[name], this.metStructor);
   };
-/*const structure = {
-  metStructor: {
-    reverseC(){return this.enCodetoObject.toReversed().map((k,i)=>k.map((l,j)=>this.enCodetoObject[i][j]))},
-    reverseR(){return this.enCodetoObject.map((k,i)=>k.toReversed().map((l,j)=>this.enCodetoObject[i][j]))},
-    tranportI(){return this.enCodetoObject.map((k,i)=>k.map((l,j)=>this.enCodetoObject[j][i]))},
-    tranportU(){return this.enCodetoObject.toReversed().map((k,i)=>k.toReversed().map((l,j)=>this.enCodetoObject[j][i]))},
-    rotate90(){return this.enCodetoObject.toReversed().map((k,i)=>k.map((l,j)=>this.enCodetoObject[j][i]))},
-    rotate180(){return this.enCodetoObject.toReversed().map((k,i)=>k.toReversed().map((l,j)=>this.enCodetoObject[i][j]))},
-    rotate270(){return this.enCodetoObject.map((k,i)=>k.toReversed().map((l,j)=>this.enCodetoObject[j][i]))},
-  },
-  conStructorB(name,enCodetoObject,configuration) {
-    this[name] = {
-      enCodetoObject,
-      configuration,
-    };
-    Object.assign(this[name], this.metStructor);
-  },
-  conStructorR(name,proto) {
-    this[name] = {
-      enCodetoObject: proto[0],
-      configuration: proto[1],
-      limit: proto[2]||undefined,
-      wall: {
-        front: proto[3]||undefined,
-        right: proto[4]||undefined,
-        back: proto[5]||undefined,
-        left: proto[6]||undefined,
-      },
-    };
-    Object.assign(this[name], this.metStructor);
-  }
-};*/
 const structure = new NFSTR();
 structure.conStructorR(("smallRoad"),[[
   [0],],
@@ -74,11 +43,12 @@ structure.conStructorR(("smallRoad"),[[
   [[-1,0],[-1,-1],[0,-1],[1,-1],[1,0]],
   [[0,-1],[-1,-1],[-1,0],[-1,1],[0,1]],
   [[-1,0],[-1,1],[0,1],[1,1],[1,0]],
+  [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]],
 ]);
 structure.conStructorR(("mediumRoad"),[[
-  [1],[1],[1],[1]
-  [1],[0],[0],[1]
-  [1],[0],[0],[1]
+  [1],[1],[1],[1],
+  [1],[0],[0],[1],
+  [1],[0],[0],[1],
   [1],[1],[1],[1]],
   [1,[true,true,true,true]],
 ]);
@@ -87,8 +57,7 @@ structure.conStructorR(("largeRoad"),[[
   [1],[0],[0],[0],[1],
   [1],[0],[0],[0],[1],
   [1],[0],[0],[0],[1],
-  [1],[1],[1],[1],[1],
-  ],
+  [1],[1],[1],[1],[1],],
   [1,[true,true,true,true]]
 ]);
 structure.conStructorR(("megaRoad"),[[
@@ -97,8 +66,7 @@ structure.conStructorR(("megaRoad"),[[
   [1],[0],[0],[0],[0],[1],
   [1],[0],[0],[0],[0],[1],
   [1],[0],[0],[0],[0],[1],
-  [1],[1],[1],[1],[1],[1],
-  ],
+  [1],[1],[1],[1],[1],[1],],
   [1,[true,true,true,true]]
 ]);
 structure.conStructorB(("basicSquare"),[[
@@ -138,8 +106,7 @@ structure.conStructorB(("prison"),[[
   [[0,[[16,5]],[50],[2]],
   [1,[true,true,true,true]],]
 ]);
-
-const map = { 0: { 0: 2 } };
+const map = {"-1": {}, "0": { "0": 2 }, "1": {}, };
 const Cmap = [[0, 0]];
 const branchRoad = [1, 10, 30];
 const whichPath = [
@@ -154,80 +121,69 @@ branchRoad.plus = function (a) {
   }
 };
 branchRoad.mins = function (a) {
-  //console.log(this[a],"you can");
   this[a] = 10;
-  //console.log(this[a],"you can");
 };
 branchRoad.get = function () {
   return [this[0], this[0] + this[1], this[0] + this[1] + this[2]];
 };
 rng.branchRoad = function (a) {
-  //console.log(a);
   const ranD = this.inRange(1, 100+(100*(Cmap.length-1))), indeks = branchRoad.get();
-  console.log("value", ...indeks, ranD);
   for (let i = 4-a; i < 3; i++) {
     if (indeks[i] > ranD) {
       branchRoad.mins(i);
-      console.log("honey",i);
       return 4 - i;
     }
   }
   return 1;
 };
-
-//restartMap();
-function createMAp(a) {
-  const current = "smallRoad";
+const whichMany = [[],[]];
+whichMany.plusing = [];
+const current = "smallRoad";
+for (const i of structure[current].walls) map[i[0]][i[1]] = 1;
+restartMap();
+function createMAp() {
   console.clear();
-  const plusing = [];
   for (const [k, v] of Cmap.entries()) {
     for (let i = structure[current].limit[0]; i <= structure[current].limit[1]; i++) 
     if (!map?.[v[0] + i]) map[v[0] + i] = {};
-    const whichMany = [];
     for (let j = 0, i = whichPath[j]; j < 4; j++, i = whichPath[j]) {
-      //const bool = 
-      //(map[v[0] + i[0]][v[1] + i[1]] != 2)&&(
-      //);
-      //bool
-      if (
+      if (map[v[0] + i[0]][v[1] + i[1]] != 2) {
+        if (
         (map[v[0] + i[0]][v[1] + i[1] + 1] == 2)? (
           (map[v[0] + i[0]][v[1] + i[1] - 1] == 2)? (
-            ((map[v[0] + i[0] + 1][v[1] + i[1]] != 2 || (map[v[0] + i[0] + 1][v[1] + i[1] + 1] != 2 && map[v[0] + i[0] + 1][v[1] + i[1] - 1] != 2)) && (map[v[0] + i[0] - 1][v[1] + i[1]] != 2 || (map[v[0] + i[0] - 1][v[1] + i[1] + 1] != 2 && map[v[0] + i[0] - 1][v[1] + i[1] - 1] != 2)))
-          ):(
-            ((map[v[0] + i[0] + 1][v[1] + i[1]] != 2 || map[v[0] + i[0] + 1][v[1] + i[1] + 1] != 2) && (map[v[0] + i[0] - 1][v[1] + i[1]] != 2 || map[v[0] + i[0] - 1][v[1] + i[1] + 1] != 2))
+            (map[v[0] + i[0] + 1][v[1] + i[1]] != 2 || (map[v[0] + i[0] + 1][v[1] + i[1] + 1] != 2 && map[v[0] + i[0] + 1][v[1] + i[1] - 1] != 2)) && (map[v[0] + i[0] - 1][v[1] + i[1]] != 2 || (map[v[0] + i[0] - 1][v[1] + i[1] + 1] != 2 && map[v[0] + i[0] - 1][v[1] + i[1] - 1] != 2))
+          ): (
+            ((map[v[0] + i[0] + 1][v[1] + i[1]] != 2 || map[v[0] + i[0] + 1][v[1] + i[1] + 1] != 2) && (map[v[0] + i[0] - 1][v[1] + i[1]] != 2 || map[v[0] + i[0] - 1][v[1] + i[1] + 1] != 2)) &&
+            ((map[v[0] + i[0] + 1][v[1] + i[1]] == 2 || map[v[0] + i[0] + 1][v[1] + i[1] - 1] != 2) && (map[v[0] + i[0] - 1][v[1] + i[1]] == 2 || map[v[0] + i[0] - 1][v[1] + i[1] - 1] != 2))
           )
-        ):(
-          (map[v[0] + i[0]][v[1] + i[1] - 1] != 2)|| (
-            ((map[v[0] + i[0] + 1][v[1] + i[1]] != 2 || map[v[0] + i[0] + 1][v[1] + i[1] - 1] != 2) && (map[v[0] + i[0] - 1][v[1] + i[1]] != 2 || map[v[0] + i[0] - 1][v[1] + i[1] - 1] != 2))
+        ): (
+          (map[v[0] + i[0]][v[1] + i[1] - 1] == 2)? (
+            ((map[v[0] + i[0] + 1][v[1] + i[1]] != 2 || map[v[0] + i[0] + 1][v[1] + i[1] - 1] != 2) && (map[v[0] + i[0] - 1][v[1] + i[1]] != 2 || map[v[0] + i[0] - 1][v[1] + i[1] - 1] != 2)) &&
+            ((map[v[0] + i[0] + 1][v[1] + i[1]] == 2 || map[v[0] + i[0] + 1][v[1] + i[1] + 1] != 2) && (map[v[0] + i[0] - 1][v[1] + i[1]] == 2 || map[v[0] + i[0] - 1][v[1] + i[1] + 1] != 2))
+          ): (
+            (map[v[0] + i[0] + 1][v[1] + i[1]] == 2 || (map[v[0] + i[0] + 1][v[1] + i[1] + 1] != 2 && map[v[0] + i[0] + 1][v[1] + i[1] - 1] != 2)) && (map[v[0] + i[0] - 1][v[1] + i[1]] == 2 || (map[v[0] + i[0] - 1][v[1] + i[1] + 1] != 2 && map[v[0] + i[0] - 1][v[1] + i[1] - 1] != 2))
           )
-        )
-      ) whichMany.push([v[0] + i[0], v[1] + i[1], i[2]]);
+        )) whichMany[0].push([v[0] + i[0], v[1] + i[1], i[2]]);
+      } else whichMany[1].push([v[0] + i[0], v[1] + i[1], i[2]]);
     }
-    //console.log(Cmap[k]);
-    if (whichMany.length == 0) {
-      Cmap.splice(k, 1);
-      //console.log("kena",Cmap[k]);
-      continue;
+    if (whichMany[0].length == 0) {
+      if (Cmap.length > 2) {
+        Cmap.splice(k, 1);
+        continue;
+      } else {
+        whichMany.shift();
+      }
     }
-    //console.log("Cmap",...Cmap);
-    //console.log("lanjut",Cmap[k]);
-    //console.log(branchRoad[0],branchRoad[1],branchRoad[2],"value");
-    //console.log(whichMany, "kuuuk");
-    //const hmany = ;
-    //console.log(hmany, "kqk");
-    //const meH = rng.sample(whichMany, rng.branchRoad(whichMany.length));
-    //console.log(hmany, ...meH);
-    //console.log(branchRoad[0],branchRoad[1],branchRoad[2],"value");
-    for (const [i,x] of Object.entries(rng.sample(whichMany, rng.branchRoad(whichMany.length)))) {
+    for (const [i,x] of Object.entries(rng.sample(whichMany[0], rng.branchRoad(whichMany[0].length)))) {
       map[x[0]][x[1]] = 2;
-      for (const i of structure[current].wall[x[2]]) if (!map[v[0] + i[0]][v[1] + i[1]]) map[v[0] + i[0]][v[1] + i[1]] = 1;
+      for (const i of structure[current].walls) if (!map[x[0] + i[0]][x[1] + i[1]]) map[x[0] + i[0]][x[1] + i[1]] = 1;
       if (i == 0) Cmap[k] = x;
-      else plusing.push(x);
+      else whichMany.plusing.push(x);
     }
+    whichMany[0] = [];
+    whichMany[1] = [];
   }
-  //console.log(branchRoad[2],"you can");
-  Cmap.push(...plusing);
-  //console.log(Cmap);
+  Cmap.push(...whichMany.plusing);
+  whichMany.plusing = [];
   branchRoad.plus(1/(Cmap.length||1));
 }
-
