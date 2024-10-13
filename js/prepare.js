3dd3d9298dbcd3f24162c64b1b3558d5f7b1dc22
Maulana-Ramadhan <maulanaramadhan3010@gmail.com>
2024-10-13 09:45:19 +0700

    update js/prepare.js

--- js/prepare.js
+++ js/prepare.js
@@ -110,18 +110,3 @@ fbg.onAuthStateChanged(fbg.auth, (user) => {
 });
 
 
-fbg.get(fbg.ref(fbg.database, 'data/thatIn')).then((sp) => {
-      window.wkwk = sp;
-      sp.forEach( i => { i = i.val(); if (i.status && i.uid != muid[1]) {
-        const el = document.createElement("div");
-        el.id = i.uid;
-        el.classList.add("players");
-        el.style.backgroundColor = i.color;
-        MainGame.appendChild(el);
-        fbg.onValue(fbg.ref(fbg.database, 'data/users/' + i.uid), (spm) => {
-          const me = spm.val();
-          el.style.transform = `translate(${me.position[0]}px, ${me.position[1]}px)`;
-          moveDirection(me.direction,el);
-        });
-      }});
-    }).catch(console.error);
