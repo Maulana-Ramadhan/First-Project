90536cf54ce1ab38ae5ab01c0b92d10b366f798d
Maulana-Ramadhan <maulanaramadhan3010@gmail.com>
2024-10-13 10:05:06 +0700

    update js/prepare.js

--- js/prepare.js
+++ js/prepare.js
@@ -109,6 +109,66 @@ function online() {
       fbg.set(fbg.ref(fbg.database, 'data/thatIn/' + user.uid + 'status'), false);
     }
   });*/
+  fbg.onAuthStateChanged(fbg.auth, (user) => {
+    console.log(user);
+    if (user) {
+      if (!localStorage.getItem("myData")) {
+        muid[0] = inputName.value;
+        muid[1] = user.uid;
+        muid[2] = PickColor.value;
+        localStorage.setItem("myData",JSON.stringify({name:muid[0],uid:muid[1],color:muid[2]}));
+        const el = document.createElement("div");
+        el.id = muid[0];
+        el.classList.add("players");
+        el.style.backgroundColor = muid[2];
+        MainGame.appendChild(el);
+        elPlayers[muid[1]] = el;
+      }
+      fbg.set(fbg.ref(fbg.database, 'data/users/' + user.uid), {
+        position: [0,0],
+        direction: 0,
+      });
+      console.log(muid);
+      fbg.set(fbg.ref(fbg.database, 'data/thatIn/' + user.uid), {
+        status: true,
+        name: muid[0],
+        uid: muid[1],
+        color: muid[2],
+      });
+      fbg.get(fbg.ref(fbg.database, 'data/thatIn')).then((sp) => {
+        window.wkwk = sp;
+        sp.forEach( i => { i = i.val(); if (i.status && i.uid != muid[1]) {
+          const el = document.createElement("div");
+          el.id = i.uid;
+          el.classList.add("players");
+          el.style.backgroundColor = i.color;
+          MainGame.appendChild(el);
+          fbg.onValue(fbg.ref(fbg.database, 'data/users/' + i.uid), (spm) => {
+            const me = spm.val();
+            el.style.transform = `translate(${me.position[0]}px, ${me.position[1]}px)`;
+            moveDirection(me.direction,el);
+          });
+        }});
+      }).catch(console.error);
+      fbg.onValue(fbg.ref(fbg.database, 'data/thatIn'), (sp) => {
+        const thid = sp._node.children_.root_.key;
+        if (muid[1] != thid) {
+          const el = document.createElement("div");
+          el.id = thid;
+          el.classList.add("players");
+          el.style.backgroundColor = sp.child(thid).val();
+          MainGame.appendChild(el);
+          fbg.onValue(fbg.ref(fbg.database, 'data/users/' + thid), (spm) => {
+            const me = spm.val();
+            el.style.transform = `translate(${me.position[0]}px, ${me.position[1]}px)`;
+            moveDirection(me.direction,el);
+          });
+        }
+      });
+    } else {
+      fbg.set(fbg.ref(fbg.database, 'data/thatIn/' + user.uid + 'status'), false);
+    }
+  });
 }
 
 
