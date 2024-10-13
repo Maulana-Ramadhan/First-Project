d4bdc3f4b2bf0832e281210d47123ba4175814d1
Maulana-Ramadhan <maulanaramadhan3010@gmail.com>
2024-10-13 13:53:35 +0700

    update js/script2.js

---
+++ js/script2.js
@@ -0,0 +1,11 @@
+(function() {
+
+const opcl = {
+  cin: false,
+}
+
+OPCLcontainerInputName.addEventListener("click", a => {
+  if (opcl.cin) containerInputName.style.display = "none"
+});
+  
+}())
