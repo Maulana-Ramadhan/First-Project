f536fd14eca0f2b9581a8ba7a8acd21364613629
Maulana-Ramadhan <maulanaramadhan3010@gmail.com>
2024-10-13 14:08:40 +0700

    update js/script2.js

--- js/script2.js
+++ js/script2.js
@@ -7,14 +7,32 @@ const opcl = {
 };
 
 OPCLcontainerInputName.addEventListener("click", a => {
-  if (opcl.cin) containerInputName.style.display = "none";
-  else containerInputName.style.display = "block";
+  if (opcl.cin) {
+    containerInputName.style.display = "none";
+    opcl.cin = false;
+  }
+  else {
+    containerInputName.style.display = "block";
+    opcl.cin = true
+  }
 });
 OPCLmyWorld.addEventListener("click", a => {
+  if (opcl.cin) {
+    containerInputName.style.display = "none";
+  }
+  else {
+    containerInputName.style.display = "block";
+  }
   if (opcl.mw) myWorld.style.display = "none";
   else myWorld.style.display = "block";
 });
 OPCLjoinWorld.addEventListener("click", a => {
+  if (opcl.cin) {
+    containerInputName.style.display = "none";
+  }
+  else {
+    containerInputName.style.display = "block";
+  }
   if (opcl.jw) joinWorld.style.display = "none";
   else joinWorld.style.display = "block";
 });
