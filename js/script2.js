4ff8021e3069b9b0a8865ee8432b0db2f80c2995
Maulana-Ramadhan <maulanaramadhan3010@gmail.com>
2024-10-13 17:26:59 +0700

    update js/script2.js

--- js/script2.js
+++ js/script2.js
@@ -45,7 +45,7 @@ OPCLjoinWorld.addEventListener("click", a => {
     newWorldNumber.value = "";
   });
   createNewWorld.addEventListener("click", a => {
-    if (newWorldName.value != "") new NewMap(newWorldName.value,newWorldNumber.value);
+    if (newWorldName.value != "") All.map[newWorldName.value] = new NewMap(newWorldName.value,newWorldNumber.value);
   });
 });
   
