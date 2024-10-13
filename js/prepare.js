bf13672a9480632d6323d9ae97441dd6f43a1790
Maulana-Ramadhan <maulanaramadhan3010@gmail.com>
2024-10-13 10:19:22 +0700

    update js/prepare.js

--- js/prepare.js
+++ js/prepare.js
@@ -133,18 +133,21 @@ function online() {
       });*/
       fbg.get(fbg.ref(fbg.database, 'data/users')).then((sp) => {
         window.wkwk = sp;
-        sp.forEach( i => { i = i.val(); if (i.status && i.uid != muid[1]) {
-          const el = document.createElement("div");
-          el.id = i.uid;
-          el.classList.add("players");
-          el.style.backgroundColor = i.color;
-          MainGame.appendChild(el);
-          fbg.onValue(fbg.ref(fbg.database, 'data/users/' + i.uid), (spm) => {
-            const me = spm.val();
-            el.style.transform = `translate(${me.position[0]}px, ${me.position[1]}px)`;
-            moveDirection(me.direction,el);
-          });
-        }});
+        sp.forEach( i => { 
+          i = i.val(); 
+          if (i.status && i.uid != muid[1]) {
+            const el = document.createElement("div");
+            el.id = i.uid;
+            el.classList.add("players");
+            el.style.backgroundColor = i.color;
+            MainGame.appendChild(el);
+            fbg.onValue(fbg.ref(fbg.database, 'data/users/' + i.uid), (spm) => {
+              const me = spm.val();
+              el.style.transform = `translate(${me.position[0]}px, ${me.position[1]}px)`;
+              moveDirection(me.direction,el);
+            });
+          }
+        });
       }).catch(console.error);
       fbg.onValue(fbg.ref(fbg.database, 'data/thatIn'), (sp) => {
         const thid = sp._node.children_.root_.key;
