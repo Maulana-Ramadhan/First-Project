b4db1f6982f696af3880fece506db81b46ca07c9
Maulana-Ramadhan <maulanaramadhan3010@gmail.com>
2024-10-13 14:00:26 +0700

    update js/script2.js

--- js/script2.js
+++ js/script2.js
@@ -14,7 +14,7 @@ OPCLmyWorld.addEventListener("click", a => {
   if (opcl.mw) myWorld.style.display = "none";
   else myWorld.style.display = "block";
 });
-OPCLmyWorld.addEventListener("click", a => {
+OPCLjoinWorld.addEventListener("click", a => {
   if (opcl.jw) joinWorld.style.display = "none";
   else joinWorld.style.display = "block";
 });
