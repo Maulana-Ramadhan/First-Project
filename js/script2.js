22551d14cc9a95fe451a2c97c117cc639defe956
Maulana-Ramadhan <maulanaramadhan3010@gmail.com>
2024-10-17 13:33:11 +0700

    update js/script2.js

--- js/script2.js
+++ js/script2.js
@@ -62,6 +62,7 @@ createNewWorld.addEventListener("click", a => {
       text: newWorldName.value,
       event: ["click", (a) => {
         MainGame.style.display = "none";
+        
       }]
     }));
     newWorldName.value = "";
