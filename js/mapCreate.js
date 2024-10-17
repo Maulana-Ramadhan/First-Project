b02d854160b58f6dd0bf64d41f3ab5958c082d21
Maulana-Ramadhan <maulanaramadhan3010@gmail.com>
2024-10-17 20:39:17 +0700

    update js/mapCreate.js

--- js/mapCreate.js
+++ js/mapCreate.js
@@ -78,16 +78,17 @@ structure.conStructorB(("basicSquare"),[[
   [1,[true,true,true,true]],]
 ]);
 structure.conStructorB(("treasureRoom"),[[
-  [1, 1, 1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 0, 1, 1, 1, 1],
   [1, 0, 0, 0, 0, 0, 0, 0, 1],
   [1, 0, 1, 1, 0, 1, 1, 0, 1],
   [1, 0, 1, 0, 0, 0, 1, 0, 1],
-  [1, 0, 0, 0, 3, 0, 0, 0, 1],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
   [1, 0, 1, 0, 0, 0, 1, 0, 1],
   [1, 0, 1, 1, 0, 1, 1, 0, 1],
   [1, 0, 0, 0, 0, 0, 0, 0, 1],
-  [1, 1, 1, 1, 2, 1, 1, 1, 1],],
-  [[0,[[4,0],[0,5],[8,5]],[50,50,50],[2]], //[configuration,[semua tiles yang diganti],[minimal, maksimal | yang harus diganti],[dengan tiles apa jika tidak diganti],[[tiles yang diacak],[bobot setiap tiles]]]
+  [1, 1, 1, 1, 0, 1, 1, 1, 1],],
+  [[0,[[[4,0],[0,5],[8,5]],[50,50,50],[2]]], //[configuration,[semua tiles yang diganti],[minimal, maksimal | yang harus diganti],[dengan tiles apa jika tidak diganti],[[tiles yang diacak],[bobot setiap tiles]]]
+  [0,[[[4,0],[0,5],[8,5]],[50,50,50],[2]]], //[configuration,[semua tiles yang diganti],[minimal, maksimal | yang harus diganti],[dengan tiles apa jika tidak diganti],[[tiles yang diacak],[bobot setiap tiles]]]
   [1,[true,true,true,true]],],
 ]);
 structure.conStructorB(("prison"),[[
