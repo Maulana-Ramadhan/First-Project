ccbe535f8aa63df12164b93f40b364d7ea61a7c7
Maulana-Ramadhan <maulanaramadhan3010@gmail.com>
2024-10-17 18:53:40 +0700

    update js/mapCreate.js

--- js/mapCreate.js
+++ js/mapCreate.js
@@ -185,7 +185,7 @@ class NewMap {
   }
   loadMap() {
     let i = "";
-    for (let i = 0; i < Things.length; i++) {
+    for (let i = -15; i < 15; i++) {
     for (let j = 0; j < Things.length; j++) {
       ik += createElT(ECTObc[this.map[i][j]],i*10,j*10);
     }}
