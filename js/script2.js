8da0d86c8da36770993f6deff407290bfc94cc41
Maulana-Ramadhan <maulanaramadhan3010@gmail.com>
2024-10-18 18:56:59 +0700

    update js/script2.js

--- js/script2.js
+++ js/script2.js
@@ -63,7 +63,8 @@ createNewWorld.addEventListener("click", a => {
       event: ["click", ({srcElement:el}) => {
         MainGame.style.display = "none";
         loadingScreen.style.display = "flex";
-        for (let i = 0; i < 1000; i++) AllMap[newWorldName.value].createMAp();
+        for (let i = 0; i < 1000; i++) AllMap[el.innerText].createMAp();
+        AllMap[el.innerText].loadMap();
       }]
     }));
     newWorldName.value = "";
