eada1eca49e92157f6a4e2f427762891c17cd565
Maulana-Ramadhan <maulanaramadhan3010@gmail.com>
2024-10-19 10:48:39 +0700

    update js/mapCreate.js

--- js/mapCreate.js
+++ js/mapCreate.js
@@ -199,8 +199,9 @@ class NewMap {
     /////////////////////////
     //canvasMap.innerHTML = innerHTML;
     Object.values(elPlayers).forEach((a) => {
-        ctx.fillRect(0,0,10,10);
         ctx.fillStyle = a[0];
+        ctx.fillRect(0,0,10,10);
+        ctx.fill();
       //canvasMap.appendChild(a);
     });
   }
